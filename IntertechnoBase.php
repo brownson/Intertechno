f866adb2c64fe7e94d8e46bc34d6e2ed81e7bd2b
Brownson <ab@brownson.at>
2016-08-07 00:11:04 +0200

    Removed Comments

--- IntertechnoBase.php
+++ IntertechnoBase.php
@@ -8,8 +8,6 @@ class IntertechnoBase extends IPSModule
 		parent::Create();
 
 		//Register Variables
-		//$this->RegisterVariableBoolean("STATE", "STATE", "~Switch");
-		//$this->EnableAction("STATE");
 		$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
 	}
 
